98cb126250343a29523ba1dda3974b0ebaeb8d82
Data Mentor <ehsanjaved302@gmail.com>
2024-11-17 08:04:52 +0500

    "Removed console logs from fetchDepartments function in JobList component"

--- src/components/Jobs/JobList.jsx
+++ src/components/Jobs/JobList.jsx
@@ -32,11 +32,7 @@ const JobList = () => {
         const fetchDepartments = async () => {
             try {
                 const response = await axios.get("http://127.0.0.1:8000/jobs/DepartmentForJobs/");
-                setDepartments(response.data);
-                console.log(response.data);
-                console.log(departments);
-                
-                
+                setDepartments(response.data);              
             } catch (error) {
                 console.error("Error fetching departments:", error);
             }
